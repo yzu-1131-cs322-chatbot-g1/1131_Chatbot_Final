ee0b25e5682bd0669014e664ce5655ffdcd29de1
Charles Chang <80582567+idzbg@users.noreply.github.com>
2024-11-29 02:25:51 +0800

    Refactor config.py

--- modules/config.py
+++ modules/config.py
@@ -1,20 +1,8 @@
+"""
+從 config.ini 讀取設定檔，並且可以在任何地方 import config 取得設定值。
+"""
+
 import configparser
 
-# 全域唯一字典
-config = {}
-
-def load_config(file_path="config.ini"):
-    """
-    讀取 config.ini 並將資料存入全域字典 config
-    """
-    print(f'Loading config from {file_path}')
-
-    global config
-    parser = configparser.ConfigParser()
-    parser.read(file_path, encoding="utf-8")
-
-    for section in parser.sections():
-        config[section] = {key: parser[section][key] for key in parser[section]}
-
-# 初始化配置
-load_config()
+config = configparser.ConfigParser()
+config.read("config.ini")
